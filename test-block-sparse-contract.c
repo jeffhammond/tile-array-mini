c9b4e29c4809e1d189e84ad5ac6d88f969ec8e3a
Jeff Hammond <jeff.science@gmail.com>
2015-04-03 16:36:24 -0700

    sparse contract now self-verifies against DGEMM

--- test-block-sparse-contract.c
+++ test-block-sparse-contract.c
@@ -1,5 +1,6 @@
 #include <unistd.h> /* getpagesize() */
 #include <math.h>
+#include <assert.h>
 
 #include "tile-blas.h"
 
@@ -33,15 +34,15 @@ int main(int argc, char * argv[])
      *    |000X|
      *    +----+
      *              */
-    int ntiles = 6;
-    int tilesdim = 4;
+    const int ntiles = 6;
+    const int tilesdim = 4;
     ssize_t block_offset[4][4] = {{ 0, 1,-1,-1},
                                   { 2, 3,-1,-1},
                                   {-1,-1, 4,-1},
                                   {-1,-1,-1, 5}};
 #else
-    int ntiles = 54;
-    int tilesdim = 12;
+    const int ntiles = 54;
+    const int tilesdim = 12;
     ssize_t block_offset[12][12] = {{ 0, 1, 2, 3, 4, 5,-1,-1,-1,-1,-1,-1},
                                     { 6, 7, 8, 9,10,11,-1,-1,-1,-1,-1,-1},
                                     {12,13,14,15,16,17,-1,-1,-1,-1,-1,-1},
@@ -70,9 +71,9 @@ int main(int argc, char * argv[])
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    double * t_a = malloc(count * sizeof(double));
-    double * t_b = malloc(count * sizeof(double));
-    double * t_c = malloc(count * sizeof(double));
+    double * t_a = malloc(count * sizeof(double)); assert(t_a!=NULL);
+    double * t_b = malloc(count * sizeof(double)); assert(t_b!=NULL);
+    double * t_c = malloc(count * sizeof(double)); assert(t_c!=NULL);
 
     long counter = 0;
     long taskid  = 0;
@@ -83,7 +84,9 @@ int main(int argc, char * argv[])
           for (int k=0; k<tilesdim; k++) {
             if ((block_offset[i][k] >= 0) && (block_offset[k][j] >= 0)) {
               if (counter==taskid) {
+#if DEBUG_LEVEL>1
                 printf("rank %d counter %ld taskid %ld (%d,%d,%d)\n", me, counter, taskid, i, j, k); fflush(stdout);
+#endif
                 memset(t_a, 0, count*sizeof(double));
                 memset(t_b, 0, count*sizeof(double));
                 memset(t_c, 0, count*sizeof(double));
@@ -92,11 +95,13 @@ int main(int argc, char * argv[])
 
                 matmul(tilesize, tilesize, tilesize, t_a, t_b, t_c, false);
 
+#if DEBUG_LEVEL>2
                 char label[8];
                 sprintf(label, "%d", me);
                 ta_print_tile(label, count, t_a);
                 ta_print_tile(label, count, t_b);
                 ta_print_tile(label, count, t_c);
+#endif
 
                 ta_sum_tile(g_c, block_offset[i][j], t_c);
                 cntr_fadd(nxtval, 1, &counter);
@@ -109,13 +114,91 @@ int main(int argc, char * argv[])
     }
     ta_sync_array(g_c);
 
+#if DEBUG_LEVEL>2
     if (tilesize<50) ta_print_array(g_c);
+#endif
 
     free(t_a);
     free(t_b);
     free(t_c);
 
     cntr_destroy(&nxtval);
+
+    /* begin correctness checking */
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (me==0) {
+      const int matrixdim = tilesdim * tilesize;
+      double * restrict m_a = calloc(matrixdim*matrixdim*sizeof(double),sizeof(double));
+      double * restrict m_b = calloc(matrixdim*matrixdim*sizeof(double),sizeof(double));
+      double * restrict m_c = calloc(matrixdim*matrixdim*sizeof(double),sizeof(double));
+      double * restrict m_d = calloc(matrixdim*matrixdim*sizeof(double),sizeof(double));
+      if (m_a==NULL || m_b==NULL || m_c==NULL || m_d==NULL) {
+        printf("cannot allocate dense matrices on 1 process - correctness checking skipped\n");
+        if (m_a!=NULL) free(m_a);
+        if (m_b!=NULL) free(m_b);
+        if (m_c!=NULL) free(m_c);
+        if (m_d!=NULL) free(m_d);
+      } else {
+        double * restrict t_a = malloc(count * sizeof(double));
+        double * restrict t_b = malloc(count * sizeof(double));
+        double * restrict t_c = malloc(count * sizeof(double));
+        /* gather tiles into buffer and copy into matrix manually 
+         * because we do not have GA-style API for local strided */
+        for (int i=0; i<tilesdim; i++) {
+          for (int j=0; j<tilesdim; j++) {
+            if (block_offset[i][j] >= 0) {
+              /* fetch tile */
+              ta_get_tile(g_a, block_offset[i][j], t_a);
+              ta_get_tile(g_b, block_offset[i][j], t_b);
+              ta_get_tile(g_c, block_offset[i][j], t_c);
+              /* copy into matrix */
+              for (int ii=0; ii<tilesize; ii++) {
+                for (int jj=0; jj<tilesize; jj++) {
+                  const int row = i*tilesize + ii;
+                  const int col = j*tilesize + jj;
+                  m_a[row*matrixdim+col] = t_a[ii*tilesize+jj]; 
+                  m_b[row*matrixdim+col] = t_b[ii*tilesize+jj]; 
+                  m_c[row*matrixdim+col] = t_c[ii*tilesize+jj]; 
+                }
+              }
+            }
+          }
+        }
+        free(t_a);
+        free(t_b);
+        free(t_c);
+        matmul(matrixdim, matrixdim, matrixdim, m_a, m_b, m_d, false);
+        size_t errors = 0;
+        for (int i=0; i<matrixdim; i++) {
+          for (int j=0; j<matrixdim; j++) {
+            errors += (m_c[i*matrixdim+j] != m_d[i*matrixdim+j]);
+          }
+        }
+        printf("%zu errors\n", errors);
+        free(m_a);
+        free(m_b);
+        free(m_c);
+        free(m_d);
+        assert(errors==0);
+      }
+    }
+#if 0
+              /* print, including the zeros */
+              for (int ii=0; ii<tilesize; ii++) {
+                for (int jj=0; jj<tilesize; jj++) {
+                    const int row = i*tilesize + ii;
+                    const int col = j*tilesize + jj;
+                    printf("m_a(%d,%d) = %lf\n", row, col, m_a[row*matrixdim+col]);
+                }
+              }
+              fflush(stdout);
+#endif
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    /* end correctness checking */
 
     ta_destroy(&g_a);
     ta_destroy(&g_b);
